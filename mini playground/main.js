a59325b46f392b11a4293944dcfbe0d47877e2f4
Khary Williamson <kharywilliamson89@gmail.com>
2022-09-26 19:27:21 -0500

    first commit

---
+++ mini playground/main.js
@@ -0,0 +1,24 @@
+window.onload (bigBuilder);
+
+const textInput = document.getElementById('addFriend');
+const mainButton = document.getElementById('addNew');
+let table = document.getElementById('output');
+const mainMessage = document.getElementById('message');
+
+const myArray = ["Jai", "Dion", "Yeon", "Mike", "MeMee", 
+"Re'Neeka", "Reen", "Shaela", "Terrance", "Rod", "Chris", "Josh", "Khary"];
+
+addNew.onclick = function () {
+   const noNewFriends = textInput.ariaValueMax;
+   myArray.push(noNewFriends);
+}
+ 
+function bigBuilder() {
+    myArray.forEach((item, index) => {
+        "";
+    }
+}
+
+function pageBuilder() {
+
+}
