92281d25c6ca9d0b0ef037d4e529d4c5f7997d52
Khary Williamson <kharywilliamson89@gmail.com>
2022-09-26 19:48:04 -0500

    A branch for homework
    tried to create for loop to dynamically create table

--- mini playground/main.js
+++ mini playground/main.js
@@ -1,15 +1,15 @@
 window.onload (bigBuilder);
-
+// these variable are to get my elements from the HTML
 const textInput = document.getElementById('addFriend');
 const mainButton = document.getElementById('addNew');
 let table = document.getElementById('output');
 const mainMessage = document.getElementById('message');
-
+// my array of names to add to the page
 const myArray = ["Jai", "Dion", "Yeon", "Mike", "MeMee", 
 "Re'Neeka", "Reen", "Shaela", "Terrance", "Rod", "Chris", "Josh", "Khary"];
 
-addNew.onclick = function () {
-   const noNewFriends = textInput.ariaValueMax;
+function getValue () {
+   const noNewFriends = textInput.value;
    myArray.push(noNewFriends);
 }
  
@@ -19,6 +19,13 @@ function bigBuilder() {
     }
 }
 
-function pageBuilder() {
 
+function pageBuilder() { 
+    // this loop is an attempt to dynamically create the table
+    for (let i = 1; i < 4; i++) {
+        let rows = document.createElement('tr');
+        for (let k = 1; k < 4; k++) {
+            let cells = document.createElement('td');
+        }
+    }
 }
